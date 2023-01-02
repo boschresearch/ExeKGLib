289938fb92dd4ff1d663eb037225c40ae0ee7b8d
Antonis Klironomos <antonisklironomos@gmail.com>
2023-01-02 14:33:39 +0100

    Fix run_method() input/output handling in statistic_tasks.py

--- executable_ml_kgs/classes/tasks/statistic_tasks.py
+++ executable_ml_kgs/classes/tasks/statistic_tasks.py
@@ -1,6 +1,6 @@
-from utils.task_utils.statistic_utils import *
 from classes.entity import Entity
 from classes.task import Task
+from utils.task_utils.statistic_utils import *
 
 """
 ❗ Important for contributors: See the package's README.md before extending the code's functionality.
@@ -12,35 +12,34 @@ class TrendCalculationTaskTrendCalculationMethod(Task):
         super().__init__(iri, parent_entity)
 
     def run_method(
-        self, other_task_output_dict: dict, input_data: pd.DataFrame
+            self, other_task_output_dict: dict, input_data: pd.DataFrame
     ) -> dict:
-        input = self.get_one_input(other_task_output_dict, input_data)
-        trend_calculation_result = trend_calculation(
-            input, self.has_input[0].has_source
-        )
+        input_dict = self.get_inputs(other_task_output_dict, input_data)
+        input_data = list(input_dict.values())[0]  # one input expected
+        trend_calculation_result = trend_calculation(input_data)
 
-        return self.create_output_dict({"Trend": trend_calculation_result})
+        return self.create_output_dict({"DataOutTrendCalculation": trend_calculation_result})
 
 
 class ScatteringCalculationTaskScatteringCalculationMethod(Task):
     def __init__(self, iri: str, parent_entity: Entity):
         super().__init__(iri, parent_entity)
 
-    def run_method(self, other_task_output_dict: dict, input_data: pd.DataFrame):
-        input = self.get_one_input(other_task_output_dict, input_data)
-        scattering_calculation_result = scattering_calculation(
-            input, self.has_input[0].has_source
-        )
+    def run_method(self, other_task_output_dict: dict, input_data: pd.DataFrame) -> dict:
+        input_dict = self.get_inputs(other_task_output_dict, input_data)
+        input_data = list(input_dict.values())[0]  # one input expected
+        scattering_calculation_result = scattering_calculation(input_data)
 
-        return self.create_output_dict({"Scattering": scattering_calculation_result})
+        return self.create_output_dict({"DataOutScatteringCalculation": scattering_calculation_result})
 
 
 class NormalizationTaskNormalizationMethod(Task):
     def __init__(self, iri: str, parent_entity: Entity):
         super().__init__(iri, parent_entity)
 
-    def run_method(self, other_task_output_dict: dict, input_data: pd.DataFrame):
-        input = self.get_one_input(other_task_output_dict, input_data)
-        normalization_result = normalization(input, self.has_input[0].has_source)
+    def run_method(self, other_task_output_dict: dict, input_data: pd.DataFrame) -> dict:
+        input_dict = self.get_inputs(other_task_output_dict, input_data)
+        input_data = list(input_dict.values())[0]  # one input expected
+        normalization_result = normalization(input_data)
 
-        return self.create_output_dict({"Normalization": normalization_result})
+        return self.create_output_dict({"DataOutNormalization": normalization_result})
