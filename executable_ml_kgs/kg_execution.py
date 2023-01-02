f044e97db35d7094abc90ca15419d6ca01893fb1
Antonis Klironomos <antonisklironomos@gmail.com>
2023-01-02 13:32:18 +0100

    Change example pipelines' save directory

--- executable_ml_kgs/kg_execution.py
+++ executable_ml_kgs/kg_execution.py
@@ -1,6 +1,6 @@
 from classes import ExeKG
 
 if __name__ == "__main__":
-    exe_kg = ExeKG(input_exe_kg_path="./kg/testPipeline_ml.ttl")
-    # exe_kg = ExeKG(input_exe_kg_path="./kg/testPipeline_visu.ttl")
+    exe_kg = ExeKG(input_exe_kg_path="./examples/pipelines/testPipeline_ml.ttl")
+    # exe_kg = ExeKG(input_exe_kg_path="./examples/pipelines/testPipeline_visu.ttl")
     exe_kg.execute_pipeline()
