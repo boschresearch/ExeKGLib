8b1ac1c8d745c4e946308c2bcca218ab9f934159
Antonis Klironomos <antonisklironomos@gmail.com>
2023-01-03 11:19:14 +0100

    Read KG path from command line in kg_execution.py

--- executable_ml_kgs/kg_execution.py
+++ executable_ml_kgs/kg_execution.py
@@ -1,6 +1,12 @@
+import sys
+
 from classes import ExeKG
 
 if __name__ == "__main__":
-    exe_kg = ExeKG(input_exe_kg_path="./examples/pipelines/MLPipeline.ttl")
-    # exe_kg = ExeKG(input_exe_kg_path="./examples/pipelines/VisuPipeline.ttl")
+    try:
+        exe_kg_path = sys.argv[1]
+    except IndexError:
+        exe_kg_path = "examples/pipelines/MLPipeline.ttl"
+
+    exe_kg = ExeKG(input_exe_kg_path=exe_kg_path)
     exe_kg.execute_pipeline()
